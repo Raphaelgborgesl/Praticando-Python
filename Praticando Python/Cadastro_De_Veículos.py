fcef2aaf60c8570c5aad098e96ce2a5d6572b83a
Raphaelgborgesl <raphacooper@gmail.com>
2024-11-09 11:56:28 -0300

    Add files via upload

---
+++ Praticando Python/Cadastro_De_Veículos.py
@@ -0,0 +1,7 @@
+#cadastrodeveículo
+Modelo_do_carro = input("Qual o modelo do carro:")
+Ano_do_modelo = input("ano:")
+Marca = input("marca:")
+
+print(Modelo_do_carro,Ano_do_modelo,Marca)
+print("Obrigado por cadastrar o veículo")
